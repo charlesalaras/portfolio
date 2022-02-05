a01229e4fb3fd6251cc2a361a91b5e1e72cac7c4
charlesalaras <charles.alaras@gmail.com>
2022-02-04 17:22:47 -0800

    Add data-inclusive plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,38 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "portfolio",
   },
-  plugins: ["gatsby-plugin-postcss",
+  plugins: [
+        "gatsby-plugin-postcss",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-json",
         {
             resolve: "gatsby-plugin-typescript",
             options: {
                 isTSX: true,
                 jsxPragma: "jsx",
                 allExtensions: true
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "data",
+                path: `${__dirname}/src/data`,
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "icons",
+                path: `${__dirname}/src/icons`,
             },
         },
   ],
