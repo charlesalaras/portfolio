cac5fe623f869e3620b1d629304c5988833c1a94
Charles Alaras <charles.alaras@gmail.com>
2022-09-04 11:07:48 -0700

    Add Bootstrap for Carouse

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { StaticImage } from "gatsby";
 import { motion } from "framer-motion";
+import Carousel from "react-bootstrap/Carousel";
 
 interface MediaProps {
     content: string[]
@@ -8,22 +8,18 @@ interface MediaProps {
 // FIXME: Use a dynamic image instead (how to get src from gatsby query?)
 export function Slideshow(props: MediaProps): JSX.Element {
     return(
-        <motion.div>
-            <motion.button className="slide">
-            </motion.button>
-            <motion.button className="slide">
-            </motion.button>
-            {props.content.map((source) => (
-                <StaticImage src={source}></StaticImage>
-            ))}
-        </motion.div>
+        <Carousel>
+            {props.content.map((source) => {
+                <Carousel.Item>
+                <img src={source}/>
+                </Carousel.Item>
+            })}
+        </Carousel>
     );
 }
 
 export function Video(props: MediaProps): JSX.Element {
     return(
-        <motion.div>
-            Hello!
-        </motion.div>
+        <iframe src={props.content[0]}></iframe>
     );
 }
