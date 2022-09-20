93fe35e6cf052f94de79833a9aacba05973a5bfe
Charles Alaras <charles.alaras@gmail.com>
2022-09-19 20:22:20 -0700

    Fix project view stylings

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -26,18 +26,18 @@ export default function Overview(props: OVProps): JSX.Element {
     }
 
     const tools = props.view.node.tools.map((tool) =>
-        <li key={tool.name}><img src={tool.icon}></img> {tool.name}</li>
+        <li key={tool.name}><img src={tool.icon} className="inset-icon"></img> {tool.name}</li>
     );
 
     const notes = props.view.node.notes.map((note) =>
-        <li key={note.note}><img src={note.icon}></img> {note.note}</li>
+        <li key={note.note}><img src={note.icon} className="inset-icon"></img> {note.note}</li>
     );
 
     return(
         <div className="blank project-view">
             {props.open ? 
                 <div className="inset">
-                <div className={props.view.node.category}>
+                <div className={`${props.view.node.category} innerView`}>
                     <button onClick={() => props.updater("")} className="close">
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
